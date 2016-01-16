08a919def9751d0331683309213a8f809647fd8d
Andrew Parmar <andrew.parmar@gmail.com>
2016-01-15 23:19:21 -0800

    Modified templates with url_for and added database

--- finalProject.py
+++ finalProject.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+# import jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -12,59 +13,72 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# #Fake Restaurants
+# restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+# # restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
 
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+# #Fake Menu Items
+# items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+# item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
 
 
 
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
+    restaurants = session.query(Restaurant).all()
+    return render_template('restaurants.html', restaurants = restaurants)
+
+@app.route('/restaurants/new', methods=['GET','POST'])
+def newRestaurant():
+    # if request.method == 'POST':
+    #     restaurants.append[request.form['newRest']]
+    #     return redirect(url_for('home'))
+    return render_template('newRestaurants.html')
+
+@app.route('/restaurants/<int:restaurant_id>/edit', methods=['GET','POST'])
+def editRestaurant(restaurant_id):
+    return render_template('editRestaurants.html', restaurant_id = restaurant_id)
+
+@app.route('/restaurants/<int:restaurant_id>/delete', methods=['GET','POST'])
+def deleteRestaurant(restaurant_id):
+    name = session.query(Restaurant.name).filter(Restaurant.id == restaurant_id).one()
+    return render_template('deleteRestaurants.html', restaurant_id = restaurant_id, name = name)
+
+@app.route('/restaurants/<int:restaurant_id>/menu')
+def showMenu(restaurant_id):
+    menu = session.query(MenuItem).join(MenuItem.restaurant).filter(Restaurant.id==restaurant_id)
+    return render_template('menu.html', restaurant_id = restaurant_id, menu = menu)
+
+@app.route('/restaurants/<int:restaurant_id>/menu/new', methods=['GET','POST'])
+def newMenuItem(restaurant_id):
+    return render_template('newMenuItem.html', restaurant_id = restaurant_id)
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET','POST'])
+def editMenuItem(restaurant_id, menu_id):
+    return render_template('editMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id)
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete', methods=['GET','POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id)
+
+
+# @app.route('/restaurants/JSON')
+# def showRestaurantsJSON():
+#     return restaurants
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '0.0.0.0', port=5000 )
+
+
+
+
     # restaurants = session.query(Restaurant).all()
     # output = ""
     # for rest in restaurants:
     #     output += rest.name
     #     output += "</br>"
     # return output
-    return render_template('restaurants.html', restaurants = restaurants)
-
-@app.route('/restaurants/new')
-def newRestaurant():
-    return render_template('newRestaurants.html')
-
-@app.route('/restaurants/<int:restaurant_id>/edit')
-def editRestaurant(restaurant_id):
-    return render_template('editRestaurants.html', restaurant_id = restaurant_id)
-
-@app.route('/restaurants/<int:restaurant_id>/delete')
-def deleteRestaurant(restaurant_id):
-    return render_template('deleteRestaurants.html', restaurant_id = restaurant_id)
-
-@app.route('/restaurants/<int:restaurant_id>/menu')
-def showMenu(restaurant_id):
-    return render_template('menu.html', restaurant_id = restaurant_id, items = items)
-
-@app.route('/restaurants/<int:restaurant_id>/menu/new')
-def newMenuItem(restaurant_id):
-    return render_template('newMenuItem.html', restaurant_id = restaurant_id)
-
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
-def editMenuItem(restaurant_id, menu_id):
-    return render_template('editMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id)
-
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
-def deleteMenuItem(restaurant_id, menu_id):
-    return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id)
-
-
-if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '0.0.0.0', port=5000 )
