9c3959ae86fb30603a7a5690de9d8cc6f6849c07
Andrew Parmar <andrew.parmar@gmail.com>
2016-07-02 01:10:18 -0700

    completed FB login

--- finalProject.py
+++ finalProject.py
@@ -163,8 +163,7 @@ def fbconnect():
         return response
     access_token = request.data
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-    'web']['app_id']
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
